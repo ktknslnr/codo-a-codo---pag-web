9bf84c2b832350ab6a0ba03c234a461f66141dae
Catalina Selener <catalinaselener23@gmail.com>
2024-05-16 18:17:00 -0300

    primer prueba de CaC, todo provisorio

---
+++ static/js/CaC.js
@@ -0,0 +1,39 @@
+function validardatos() {
+
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let telefono = document.getElementById("telefono").value;
+    let email = document.getElementById("mail").value;
+    let suscomentarios = document.getElementById("comentario").value;
+    let error = document.getElementById("error")
+
+    error.innerHTML = "";
+    if (nombre === "" && apellido === "" && telefono === "" && email === "") {
+        error.innerHTML = "Por favor, complete los campos requeridos"
+        return false
+    }
+
+    if (nombre === "" || /^([0-9])*$/.test(nombre)) {
+        error.innerHTML = "Por favor, ingrese un nombre válido"
+        return false
+    }
+    if (apellido === "" || /^([0-9])*$/.test(apellido)) {
+        error.innerHTML = "Por favor, ingrese un apellido válido"
+        return false
+    }
+    if (telefono === "") {
+        error.innerHTML = "Por favor, ingrese un número de télefono válido"
+        return false
+    }
+    if (email === "" ) {
+        error.innerHTML = "Por favor, ingrese una dirección de email válida"
+        return false
+    }
+    
+    else{
+        alert("Envío de formulario exitoso")
+        return true
+    }
+
+
+}
